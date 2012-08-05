7549a81d1ea6da0b291d2dfd7c0c53181b92f149
Naitik Shah <n@daaku.org>
2012-08-05 01:12:16 -0700

    formating/cleanup

--- redis.go
+++ redis.go
@@ -1,48 +1,4 @@
 // Package redis implements a client for Redis.
-//
-// Client
-//
-// The Client implements one method; Call(). This writes your command to the
-// server, then reads the subsequent reply and returns it to you. 
-//
-// The Client struct also has a pool of connections so it's safe to use a
-// client in a concurrent context. You can create one client for your entire
-// program and share it between go routines.
-//
-//      c := redis.NewClient("tcp:127.0.0.1:6379", 0, "")
-//      reply, err := c.Call("GET", "foo")
-//      if err != nil {
-//          // handle error
-//      }
-//
-//      println(reply.Elem.String())
-//
-// AsyncClient
-// 
-// The AsyncClient works exactly like the regular Client, and implements a
-// single method Call(), but this method does not return any reply, only an
-// error or nil. 
-//
-//      c := redis.NewAsyncClient("tcp:127.0.0.1:6379")
-//      err := c.Call("SET", "foo", 1)
-//      err = c.Call("GET", "foo")
-//
-// When we send our command and arguments to the Call() method nothing is sent
-// to the Redis server. To get the reply for our commands from Redis we use the
-// Read() method. Read sends any buffered commands to the Redis server, and
-// then reads one reply. Subsequent calls to Read will return more replies or
-// block if there are none.
-//
-//      // reply from SET 
-//      reply, _ := c.Read()
-//
-//      // reply from GET
-//      reply, _ = c.Read()
-//
-//      fmt.Println(reply.Elem.Int())
-// 
-// Due to the nature of how the AsyncClient works, it's not safe to share it
-// between go routines.
 package redis
 
 import (
@@ -50,9 +6,9 @@ import (
 	"strings"
 )
 
-// Client implements a Redis client which handles connections to the database
-// in a pool. The size of the pool can be adjusted with by setting the
-// MaxConnections variable before creating a client.
+// Client implements a Redis client which handles connections to the
+// database in a pool. You can create one client for your entire program
+// and share it between go routines.
 type Client struct {
 	Addr     string
 	Proto    string
@@ -77,7 +33,7 @@ func NewClient(addr string, db int, password string, max int) *Client {
 	}
 }
 
-// Call is the canonical way of talking to Redis. It accepts any 
+// Call is the canonical way of talking to Redis. It accepts any
 // Redis command and a arbitrary number of arguments.
 func (c *Client) Call(args ...interface{}) (*Reply, error) {
 	conn, err := c.connect()
@@ -93,7 +49,7 @@ func (c *Client) Call(args ...interface{}) (*Reply, error) {
 	return conn.Read()
 }
 
-// Pop a connection from pool 
+// Pop a connection from the pool.
 func (c *Client) connect() (conn Conn, err error) {
 	conn = c.pool.pop()
 	if conn == nil {
@@ -105,7 +61,7 @@ func (c *Client) connect() (conn Conn, err error) {
 	return conn, nil
 }
 
-// Use the connection settings from Client to create a new AsyncClient
+// Create a new AsyncClient.
 func (c *Client) AsyncClient() *AsyncClient {
 	return &AsyncClient{c, bytes.NewBuffer(make([]byte, 0, 1024*16)), nil, 0}
 }
@@ -113,6 +69,30 @@ func (c *Client) AsyncClient() *AsyncClient {
 // Async client implements an asynchronous client. It is very similar to Client
 // except that it maintains a buffer of commands which first are sent to Redis
 // once we explicitly request a reply.
+// The AsyncClient works exactly like the regular Client, and implements a
+// single method Call(), but this method does not return any reply, only an
+// error or nil.
+//
+//      c := redis.NewAsyncClient("tcp:127.0.0.1:6379")
+//      err := c.Call("SET", "foo", 1)
+//      err = c.Call("GET", "foo")
+//
+// When we send our command and arguments to the Call() method nothing is sent
+// to the Redis server. To get the reply for our commands from Redis we use the
+// Read() method. Read sends any buffered commands to the Redis server, and
+// then reads one reply. Subsequent calls to Read will return more replies or
+// block if there are none.
+//
+//      // reply from SET
+//      reply, _ := c.Read()
+//
+//      // reply from GET
+//      reply, _ = c.Read()
+//
+//      fmt.Println(reply.Elem.Int())
+//
+// Due to the nature of how the AsyncClient works, it's not safe to share it
+// between go routines.
 type AsyncClient struct {
 	*Client
 	buf    *bytes.Buffer
@@ -127,8 +107,8 @@ func (ac *AsyncClient) Call(args ...interface{}) (err error) {
 	return err
 }
 
-// Read does three things. 
-// 
+// Read does three things.
+//
 //      1) Open connection to Redis server, if there is none.
 //      2) Write any buffered commands to the server.
 //      3) Try to read a reply from the server, or block on read.
@@ -136,45 +116,40 @@ func (ac *AsyncClient) Call(args ...interface{}) (err error) {
 // Read returns a Reply or error.
 func (ac *AsyncClient) Read() (*Reply, error) {
 	if ac.conn == nil {
-		conn, e := NewConn(ac.Addr, ac.Proto, ac.Db, ac.Password)
-
-		if e != nil {
-			return nil, e
+		conn, err := NewConn(ac.Addr, ac.Proto, ac.Db, ac.Password)
+		if err != nil {
+			return nil, err
 		}
-
 		ac.conn = conn
 	}
 
 	if ac.buf.Len() > 0 {
 		_, err := ac.buf.WriteTo(ac.conn.Sock())
-
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	reply, e := ac.conn.Read()
+	reply, err := ac.conn.Read()
 	ac.queued--
-	return reply, e
+	return reply, err
 }
 
+// Get the count of queued commands.
 func (ac *AsyncClient) Queued() int {
 	return ac.queued
 }
 
+// Read all the queued replies.
 func (ac *AsyncClient) ReadAll() ([]*Reply, error) {
 	replies := make([]*Reply, 0, ac.queued)
-
 	for ac.Queued() > 0 {
-		r, e := ac.Read()
-
-		if e != nil {
-			return nil, e
+		reply, err := ac.Read()
+		if err != nil {
+			return nil, err
 		}
-
-		replies = append(replies, r)
+		replies = append(replies, reply)
 	}
-
 	return replies, nil
 }
 
